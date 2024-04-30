63ad3a748c5790894a4c8c93ac971d18a6da3db2
Thibaud Veron <thibaud.veron@free.fr>
2024-04-30 22:23:52 +0200

    sucessful sidebar

--- app/root.tsx
+++ app/root.tsx
@@ -4,16 +4,30 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
+import PersistentDrawerLeft from "./components/sidebar"
+import React from "react";
+import theme from "./src/theme";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+
+interface DocumentProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+export function Document({ children, title }: DocumentProps) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
+        <meta name="emotion-insertion-point" content="emotion-insertion-point" />
       </head>
       <body>
         {children}
@@ -22,8 +36,61 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </body>
     </html>
   );
-}
+};
+
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <Document>
+      <PersistentDrawerLeft>
+        <Outlet />
+      </PersistentDrawerLeft>
+    </Document>
+  )
+}
+
+
+// https://remix.run/docs/en/main/route/error-boundary
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    let message;
+    switch (error.status) {
+      case 401:
+        message = <p>Oops! Looks like you tried to visit a page that you do not have access to.</p>;
+        break;
+      case 404:
+        message = <p>Oops! Looks like you tried to visit a page that does not exist.</p>;
+        break;
+
+      default:
+        throw new Error(error.data || error.statusText);
+    }
+
+    return (
+      <Document title={`${error.status} ${error.statusText}`}>
+        <h1>
+          {error.status}: {error.statusText}
+        </h1>
+        {message}
+      </Document>
+    );
+  }
+
+  if (error instanceof Error) {
+    console.error(error);
+    return (
+      <Document title="Error!">
+        <div>
+          <h1>There was an error</h1>
+          <p>{error.message}</p>
+          <hr />
+          <p>Hey, developer, you should replace this with what you want your users to see.</p>
+        </div>
+      </Document>
+    );
+  }
+
+  return <h1>Unknown Error</h1>;
 }
